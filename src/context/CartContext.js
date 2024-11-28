7a545e2f354a0fb869b4f3ae062481fcb141f01e
Maynara Vanessa da Silva Lopes <20211074010033@ifrn.local>
2024-11-28 09:26:33 -0300

    Supabase OK!

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect, useReducer } from "react";
+import { supabase } from "../utils/supabase";
 
 export const CartContext = createContext({
     items: [],
@@ -16,21 +17,32 @@ export default function CartContextProvider({ children }) {
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        async function fetchProducts() {
+        async function getProducts() {
             setLoading(true);
-
-            const response = await fetch("https://dummyjson.com/products/category/motorcycle?limit=12&select=id,thumbnail,title,price,description");
-            if (response.ok) {
-                const result = await response.json();
-                setProducts(result.products);
+            const {data: products , error } = 
+            await supabase.from("products").select();
+            if (products.length > 0) {
+                setProducts(products);
             } else {
-                setError("Fatch FAILED!");
+                setError(`Fetcing products failed! ${error}`);
             }
-
             setLoading(false);
         }
+        getProducts();
 
-        fetchProducts();
+
+        //async function fetchProducts() {
+            //setLoading(true);
+            //const response = await fetch("https://dummyjson.com/products/category/motorcycle?limit=12&select=id,thumbnail,title,price,description");
+            //if (response.ok) {
+                //const result = await response.json();
+                //setProducts(result.products);
+            //} else {
+                //setError("Fatch FAILED!");
+            //}
+            //setLoading(false);
+       
+        //fetchProducts();
     }, [])
 
     //SHOPPING CART
